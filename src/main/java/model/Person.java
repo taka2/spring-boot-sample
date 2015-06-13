7c54b893783856b9579644b8846a826b684fcfcf
takahiro.shigemoto <takahiro.shigemoto@gmail.com>
2015-06-13 23:45:01 +0900

    Issues #2 登録項目にプルダウンを追加する

--- src/main/java/model/Person.java
+++ src/main/java/model/Person.java
@@ -4,14 +4,28 @@ public class Person {
 	private Integer id;
 	private String name;
 	private Integer age;
+	private String country;
 
 	public Person() {
 		
 	}
-	public Person(Integer id, String name, Integer age) {
+	public Person(Integer id, String name, Integer age, String country) {
 		this.id = id;
 		this.name = name;
 		this.age = age;
+		this.country = country;
+	}
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(this.id);
+		sb.append(":");
+		sb.append(this.name);
+		sb.append(":");
+		sb.append(this.age);
+		sb.append(":");
+		sb.append(this.country);
+		
+		return sb.toString();
 	}
 	public Integer getId() {
 		return id;
@@ -30,5 +44,11 @@ public class Person {
 	}
 	public void setAge(Integer age) {
 		this.age = age;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
 	}
 }
--- src/main/java/service/PersonService.java
+++ src/main/java/service/PersonService.java
@@ -8,8 +8,8 @@ import model.Person;
 public class PersonService {
 	private static List<Person> persons = new ArrayList<Person>();
 	static {
-    	persons.add(new Person(1, "hoge", 25));
-    	persons.add(new Person(2, "moge", 35));
+    	persons.add(new Person(1, "hoge", 25, "Japan"));
+    	persons.add(new Person(2, "moge", 35, "US"));
 	}
 
 	public static List<Person> getPersons() {
