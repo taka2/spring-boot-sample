8d28e1cb0f99700c085e86053c957f69116c4e27
takahiro.shigemoto <takahiro.shigemoto@gmail.com>
2015-06-13 23:33:10 +0900

    メソッド名を修正。

--- src/main/java/web/PersonsController.java
+++ src/main/java/web/PersonsController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import model.Person;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,7 +15,7 @@ import service.PersonService;
 public class PersonsController {
 
     @RequestMapping("/persons")
-    public String helo(Model model, @ModelAttribute("form") PersonsForm form) {
+    public String persons(@ModelAttribute("form") PersonsForm form) {
     	// Person一覧を取得
     	List<Person> persons = PersonService.getPersons();
     	// formにセット
@@ -26,7 +25,7 @@ public class PersonsController {
     }
 
     @RequestMapping(value = "/registerPersons", method = RequestMethod.POST)
-    public String personList(@ModelAttribute("form") PersonsForm form) {
+    public String registerPersons(@ModelAttribute("form") PersonsForm form) {
     	// submitされた値を表示
     	for(Person person : form.getPersons()) {
     		System.out.println(person.getId() + ":" + person.getName() + ":" + person.getAge());
