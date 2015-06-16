633c552167d7810c64dc8d3bcdf53fa0e56e031e
takahiro.shigemoto <takahiro.shigemoto@gmail.com>
2015-06-16 23:01:58 +0900

    Issues #13 入力項目の横スクロール固定

--- src/main/java/service/PersonService.java
+++ src/main/java/service/PersonService.java
@@ -8,8 +8,10 @@ import model.Person;
 public class PersonService {
 	private static List<Person> persons = new ArrayList<Person>();
 	static {
-    	persons.add(new Person(1, "hoge", 25, "Japan"));
-    	persons.add(new Person(2, "moge", 35, "US"));
+    	persons.add(new Person(1, "Taro", 25, "Japan"));
+    	persons.add(new Person(2, "Bob", 35, "US"));
+    	persons.add(new Person(3, "Hanako", 45, "Japan"));
+    	persons.add(new Person(4, "Nancy", 55, "US"));
 	}
 
 	public static List<Person> getPersons() {
