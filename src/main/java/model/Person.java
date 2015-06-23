a31761d95164df0b0aef662478082d0de99fe8bc
takahiro.shigemoto <takahiro.shigemoto@gmail.com>
2015-06-23 17:39:34 +0900

    Issues #19 テーブルレイアウトコンポーネントの再検討 チェックボックス項目の追加、チェック状態でのソート

--- src/main/java/model/Person.java
+++ src/main/java/model/Person.java
@@ -8,16 +8,18 @@ public class Person {
 	private Long age;
 	private String country;
 	private String city;
+	private boolean check;
 
 	public Person() {
 		
 	}
-	public Person(Long id, String name, Long age, String country, String city) {
+	public Person(Long id, String name, Long age, String country, String city, Boolean check) {
 		this.id = id;
 		this.name = name;
 		this.age = age;
 		this.country = country;
 		this.city = city;
+		this.check = check;
 	}
 	public Person(Map<String, Object> map) {
 		this.id = (Long)map.get("id");
@@ -25,6 +27,7 @@ public class Person {
 		this.age = (Long)map.get("age");
 		this.country = (String)map.get("country");
 		this.city = (String)map.get("city");
+		this.check = (Boolean)map.get("check");
 	}
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
@@ -37,6 +40,8 @@ public class Person {
 		sb.append(this.country);
 		sb.append(":");
 		sb.append(this.city);
+		sb.append(":");
+		sb.append(this.check);
 		
 		return sb.toString();
 	}
@@ -69,5 +74,11 @@ public class Person {
 	}
 	public void setCity(String city) {
 		this.city = city;
+	}
+	public boolean isCheck() {
+		return check;
+	}
+	public void setCheck(boolean check) {
+		this.check = check;
 	}
 }
--- src/main/java/service/PersonService.java
+++ src/main/java/service/PersonService.java
@@ -8,10 +8,10 @@ import model.Person;
 public class PersonService {
 	private static List<Person> persons = new ArrayList<Person>();
 	static {
-    	persons.add(new Person(1L, "Taro", 25L, "Japan", "Tokyo"));
-    	persons.add(new Person(2L, "Bob", 35L, "US", "Newyork"));
-    	persons.add(new Person(3L, "Hanako", 45L, "Japan", "Osaka"));
-    	persons.add(new Person(4L, "Nancy", 55L, "US", "Cicago"));
+    	persons.add(new Person(1L, "Taro", 25L, "Japan", "Tokyo", true));
+    	persons.add(new Person(2L, "Bob", 35L, "US", "Newyork", false));
+    	persons.add(new Person(3L, "Hanako", 45L, "Japan", "Osaka", false));
+    	persons.add(new Person(4L, "Nancy", 55L, "US", "Cicago", true));
 	}
 
 	public static List<Person> getPersons() {
